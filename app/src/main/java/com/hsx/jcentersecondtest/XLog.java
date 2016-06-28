e9c5a75ba19695f1b65582b8cae9bd8942da3f7b
huangshuixiang <hsx19890903>
2016-06-28 10:43:44 +0800

    更新了

---
+++ app/src/main/java/com/hsx/jcentersecondtest/XLog.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2015 [1076559197@qq.com | tchen0707@gmail.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hsx.jcentersecondtest;
+
+import android.util.Log;
+
+/**
+ * Description: Log utils
+ */
+public class XLog {
+    /**
+     * This flag to indicate the log is enabled or disabled.
+     */
+//    private static boolean isLogEnable = true;
+    private static boolean isLogEnable = false;
+
+
+    /**
+     * Disable the log output.
+     */
+    public static void disableLog() {
+        isLogEnable = false;
+    }
+
+    /**
+     * Enable the log output.
+     */
+    public static void enableLog() {
+        isLogEnable = true;
+    }
+
+    /**
+     * @param tag reason
+     * @param msg message
+     */
+    public static void d(String tag, String msg) {
+        if (isLogEnable) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            Log.d(tag, rebuildMsg(stackTraceElement, msg));
+        }
+    }
+
+    /**
+     * @param tag reason
+     * @param msg message
+     */
+    public static void i(String tag, String msg) {
+        if (isLogEnable) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            Log.i(tag, rebuildMsg(stackTraceElement, msg));
+        }
+    }
+
+    /**
+     * @param tag reason
+     * @param msg message
+     */
+    public static void v(String tag, String msg) {
+        if (isLogEnable) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            Log.v(tag, rebuildMsg(stackTraceElement, msg));
+        }
+    }
+
+    /**
+     * @param tag reason
+     * @param msg message
+     */
+    public static void w(String tag, String msg) {
+        if (isLogEnable) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            Log.w(tag, rebuildMsg(stackTraceElement, msg));
+        }
+    }
+
+    /**
+     * @param tag reason
+     * @param msg message
+     */
+    public static void e(String tag, String msg) {
+        if (isLogEnable) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            Log.e(tag, rebuildMsg(stackTraceElement, msg));
+        }
+        label :
+        for (int i = 0; i <10 ; i++) {
+            for (int j = 0; j <10 ; i++) {
+//                break label;
+                continue label;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param tag tag
+     * @param msg msg
+     * @param t t
+     */
+    public static void e(String tag, String msg, Throwable t) {
+        if (isLogEnable) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            Log.e(tag, rebuildMsg(stackTraceElement, msg), t);
+        }
+    }
+
+    /**
+     *
+     * @param stackTraceElement stackTraceElement
+     * @param msg meaasge
+     * @return string
+     */
+    private static String rebuildMsg(StackTraceElement stackTraceElement, String msg) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(stackTraceElement.getFileName());
+        sb.append(" (");
+        sb.append(stackTraceElement.getLineNumber());
+        sb.append(") ");
+        sb.append(stackTraceElement.getMethodName());
+        sb.append(": ");
+        sb.append(msg);
+        return sb.toString();
+    }
+
+    /**
+     *
+     * @param content content
+     */
+    public static void i(String content) {
+        if (isLogEnable) {
+            Log.i("minna", content);
+        }
+    }
+}
